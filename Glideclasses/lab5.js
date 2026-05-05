52f12b2b6332ab02433aff0169f4cf7f2fcf9807
Nancy Lakra <nancylakra99@gmail.com>
2026-05-05 17:18:54 +0530

    Initial Commit

--- Glideclasses/lab5.js
+++ Glideclasses/lab5.js
@@ -1,34 +1,44 @@
 
-//Active or InProgress inicident with slas
-var incGR = new GlideRecord('incident');
+// //Active or InProgress inicident with slas
+// var incGR = new GlideRecord('incident');
 
-// Query: Active + In Progress
-incGR.addQuery('active', true);
-incGR.addQuery('state', 2); // 2 = In Progress
-incGR.query();
+// // Query: Active + In Progress
+// incGR.addQuery('active', true);
+// incGR.addQuery('state', 2); // 2 = In Progress
+// incGR.query();
 
-while (incGR.next()) {
+// while (incGR.next()) {
 
-    gs.info("Checking Incident: " + incGR.number);
+//     gs.info("Checking Incident: " + incGR.number);
 
-    // Check SLA for this incident
-    var slaGR = new GlideRecord('task_sla');
-    slaGR.addQuery('task', incGR.sys_id);
-    slaGR.query();
+//     // Check SLA for this incident
+//     var slaGR = new GlideRecord('task_sla');
+//     slaGR.addQuery('task', incGR.sys_id);
+//     slaGR.query();
 
-    while (slaGR.next()) {
+//     while (slaGR.next()) {
 
-        gs.info("SLA Name: " + slaGR.sla.name);
-        gs.info("SLA Stage: " + slaGR.stage);
+//         gs.info("SLA Name: " + slaGR.sla.name);
+//         gs.info("SLA Stage: " + slaGR.stage);
 
-        // Example condition: SLA Breached
-        if (slaGR.stage == 'breached') {
+//         // Example condition: SLA Breached
+//         if (slaGR.stage == 'breached') {
 
-            gs.info("SLA Breached for Incident: " + incGR.number);
+//             gs.info("SLA Breached for Incident: " + incGR.number);
 
-            // Example action: Add work note
-            incGR.work_notes = "SLA has been breached. Please take action.";
-            incGR.update();
-        }
-    }
-}
+//             // Example action: Add work note
+//             incGR.work_notes = "SLA has been breached. Please take action.";
+//             incGR.update();
+//         }
+//     }
+// }
+var gr_inc = new GlideRecord('incident');
+ 
+ gr_inc.addEncodedQuery('active=true^state=2');
+ gr_inc.addJoinQuery('task_sla','sys_id','task')
+ 
+ gr_inc.query()
+ 
+ while(gr_inc.next()){
+    gs.info('Incident: ' + gr_inc.number + 'SLA Stage:' + gr_inc.state.getDisplayValue());
+ }
